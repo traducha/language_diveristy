87aeb9a06050eeca26abee932e69845658ca861b
tomaszraducha <raducha.tomasz@gmail.com>
2017-04-20 13:18:13 +0200

    paths

--- model/plot_path.py
+++ model/plot_path.py
@@ -17,7 +17,7 @@ def lin(x, a, b):
     return a * x + b
 
 
-with open('/home/tomaszraducha/Dropbox/DaneAxelrod/mgr/mgr/path/cluster_paths_q3_av48.data', 'rb') as _file:
+with open('/home/tomaszraducha/Dropbox/DaneAxelrod/mgr/mgr/path/cluster_paths_q2_av200.data', 'rb') as _file:
     path = pickle.load(_file)
 
 x, y = [], []
@@ -30,7 +30,7 @@ print popt[0]
 plt.plot(range(410, 4100, 2), lin(range(410, 4100, 2), popt[0], popt[1]), 'k-')
 plt.scatter(x, y, marker='s', s=20*2, color='blue')
 
-with open('/home/tomaszraducha/Dropbox/DaneAxelrod/mgr/mgr/path/high_k_cluster_paths_q3_av80.data', 'rb') as _file:
+with open('/home/tomaszraducha/Dropbox/DaneAxelrod/mgr/mgr/path/high_k_cluster_paths_q2_av200.data', 'rb') as _file:
     path = pickle.load(_file)
 
 x, y = [], []
@@ -44,7 +44,7 @@ print popt[0]
 plt.scatter(x, y, marker='s', s=20*2, color='orange')
 
 plt.xlim([300, 4300])
-plt.ylim([0, 26])
+plt.ylim([0, 27])
 plt.xlabel('N', fontsize=axsize)
 plt.ylabel('av. path length', fontsize=axsize)
 plt.tick_params(axis='both', which='major', labelsize=ticksize)

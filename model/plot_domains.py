3d78ecca4a020050c3f73a4a1f1e1e9a22b97067
tomaszraducha <raducha.tomasz@gmail.com>
2017-05-22 23:36:51 +0200

    wykresy na crossroads2

--- model/plot_domains.py
+++ model/plot_domains.py
@@ -10,14 +10,16 @@ import re
 import time
 import logging as log
 import numpy as np
+import matplotlib as mpl
+mpl.rcParams['font.family'] = 'serif'
 
 
 # q_list = [2, 5, 50, 100]
 N_list = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
 styles = {2: 's', 5: 'o', 50: 'D', 100: '^'}
 
-ticksize = 16
-axsize = 18
+ticksize = 9 * 0.75 / 0.5
+axsize = 12 * 0.75 / 0.5
 
 
 def fetch_results():
@@ -44,7 +46,13 @@ def fetch_results():
 
 
 def plot_results(res, qs, ymin=0, name=None):
-    for q_mode, results in res.items():
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    a_, b_, c_ = None, None, None
+    A = res.keys()
+    A = sorted(A)
+    for q_mode in A:
+        results = res[q_mode]
         print q_mode
         q = int(q_mode.split('=')[-1])
         if q not in qs:
@@ -62,27 +70,39 @@ def plot_results(res, qs, ymin=0, name=None):
             n_list.append(N)
 
         if 'high_k_cluster' in q_mode:
-            plt.scatter(n_list, com_av, color='#ffa517', marker=styles[q], s=40*2)
+            a = ax.scatter(n_list, com_av, color='#21759B', marker=styles[q], s=40*2)
+            if a_ is None:
+                a_ = True
+                a.set_label('local rewiring 2')
         elif 'cluster' in q_mode:
-            plt.scatter(n_list, com_av, color='#3591d0', marker=styles[q], s=40*2)
+            b = ax.scatter(n_list, com_av, color='#C40233', marker=styles[q], s=40*2)
+            if b_ is None:
+                b_ = True
+                b.set_label('local rewiring 1')
         elif 'normal' in q_mode:
-            plt.scatter(n_list, com_av, color='#96c824', marker=styles[q], s=40*2)
+            c = ax.scatter(n_list, com_av, color='#EE7F2D', marker=styles[q], s=40*2)
+            if c_ is None:
+                c_ = True
+                c.set_label('uniform rewiring')
+
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(loc=2, fontsize=axsize, framealpha=0.0)
     # blue 3591d0
     # yellow ffd24d
     # red f3363e
     # purple d5a6f2
     # orange ffa517
     # green 96c824
-    plt.xlabel('N', fontsize=axsize)
-    plt.ylabel('Number of domains', fontsize=axsize)
-    plt.xlim([50, 1050])
-    plt.ylim(ymin=ymin)
-    plt.tick_params(axis='both', which='major', labelsize=ticksize)
+    ax.set_xlabel('N', fontsize=axsize)
+    ax.set_ylabel('Number of domains', fontsize=axsize)
+    ax.set_xlim([50, 1050])
+    ax.set_ylim(ymin=ymin)
+    ax.tick_params(axis='both', which='major', labelsize=ticksize)
     plt.tight_layout()
 
     plt.show()
-    # plt.savefig('/home/tomaszraducha/Pulpit/{}.pdf'.format(name))
-    # plt.savefig('/home/tomaszraducha/Pulpit/{}.eps'.format(name))
+    # plt.savefig('/home/tomaszraducha/Pulpit/{}.pdf'.format(name), transparent=True)
+    # plt.savefig('/home/tomaszraducha/Pulpit/{}.eps'.format(name), transparent=True)
     plt.clf()
 
 
